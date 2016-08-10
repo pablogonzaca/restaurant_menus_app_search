742b68814d530dff0cc55ae189c138db1eff3114
Pablo Gonzalez <pablo.gonzalez.camacho@intel.com>
2016-08-10 10:17:22 -0600

    first commit: project structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restaurantMenusAppSearchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the restaurantMenusAppSearchApp
+ */
+angular.module('restaurantMenusAppSearchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
