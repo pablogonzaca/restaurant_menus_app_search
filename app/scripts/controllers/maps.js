0d09d19e93f025b7e4acd7a7fc4835d722d9374d
Pablo Gonzalez <pablo.gonzalez.camacho@intel.com>
2016-08-22 11:24:13 -0600

    add google maps support

--- app/scripts/controllers/maps.js
+++ app/scripts/controllers/maps.js
@@ -1,31 +1,89 @@
 'use strict';
 
 angular.module('restaurantMenusAppSearchApp')
-  .controller('MapasCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+  .controller('MapasCtrl', ['$scope', '$q', '$window', '$http',
+    function ($scope, $q, $window, $http) {
 
       angular.extend($scope, {
           center: {
             lat: 0,
             lng: 0,
-            zoom: 17
-          }
+            zoom: 16
+          },
+          layers: {
+            baselayers: {
+                googleTerrain: {
+                    name: 'Google Terrain',
+                    layerType: 'TERRAIN',
+                    type: 'google'
+                },
+                googleHybrid: {
+                    name: 'Google Hybrid',
+                    layerType: 'HYBRID',
+                    type: 'google'
+                },
+                googleRoadmap: {
+                    name: 'Google Streets',
+                    layerType: 'ROADMAP',
+                    type: 'google'
+                }
+            }
+          },
+          markers: {
+              m1: {
+                  lat: 10.029039113909,
+                  lng: -84.09422904253006,
+                  message: 'My house'
+              }
+          },
+          defaults: {
+            scrollWheelZoom: false
+          },
+          events: {
+            map: {
+                enable: ['click'],
+                logic: 'emit'
+            }
+        }
       });
 
-      function getPosition(){
-        $http.get('http://jsonip.com').success(function(res) {
+      function get_current_position(){
+        var deferred = $q.defer();
+        if (!$window.navigator.geolocation) {
+            deferred.reject('Geolocation not supported.');
+        } else {
+            $window.navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    deferred.resolve(position);
+                },
+                function (err) {
+                    deferred.reject(err);
+                });
+        }
+        return deferred.promise;
+      }
 
-            var url = 'http://freegeoip.net/json/' + res.ip;
-            $http.get(url).success(function(res) {
-              $scope.center = {
-                lat: res.latitude,
-                lng: res.longitude,
-                zoom: 18
-              };
-            });
+      function get_position(){
+        get_current_position().then(function(value){
+          console.log(value);
+          $scope.center = {
+            lat: value.coords.latitude,
+            lng: value.coords.longitude,
+            zoom: 16
+          };
         });
       }
 
-      getPosition();
+      get_position();
+
+      $scope.$on('leafletDirectiveMap.click', function(event, wrap){
+          console.log('DirectiveMap lat: ' + wrap.leafletEvent.latlng.lat);
+          console.log('DirectiveMap lng: ' + wrap.leafletEvent.latlng.lng);
+      });
+
+      $scope.$on('leafletDirectiveMarker.click', function(event, wrap){
+          console.log('DirectiveMarker lat: ' + wrap.leafletEvent.latlng.lat);
+          console.log('DirectiveMarker lng: ' + wrap.leafletEvent.latlng.lng);
+      });
 
   }]);
