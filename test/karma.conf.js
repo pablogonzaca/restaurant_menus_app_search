34a5c8d0db101984c00fbed9d8be507d222ac624
krojas14 <krb.14@hotmail.com>
2016-08-29 10:50:15 +0200

    first commit

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,7 +31,6 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
-      'bower_components/angular-leaflet-directive/dist/plugins/Google.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
